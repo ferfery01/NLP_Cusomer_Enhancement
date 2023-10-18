506a6004f549b9a228cfc20ca3ef1f35efa2636e
zeinab.takbiri <zeinab.takbiri@walgreens.com>
2023-10-18 12:01:01 -0500

    demo

--- unified_desktop/pipelines/intent.py
+++ unified_desktop/pipelines/intent.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Any, List, Optional, Tuple, Union
 
 import torch
 from transformers import pipeline
@@ -7,31 +7,86 @@ from unified_desktop.pipelines.base import UDBase
 
 
 class UDIntentClassification(UDBase):
+    """
+    This class is used for intent classification using the ALBERT model.
+
+    ALBERT is a transformers model pretrained on a large corpus of English data in a self-supervised fashion.
+
+    Goal: Detect the intent of the customers using their text queries.
+    """
+
     def __init__(
         self,
         task: Optional[str] = None,
         name: str = "vineetsharma/customer-support-intent-albert",
         device: Union[str, torch.device, None] = None,
     ):
+        """
+        Initialize the UDIntentClassification class.
+
+        Args:
+            task (Optional[str]): An optional task description.
+            name (str): The name of the ALBERT model to use.
+            device (Union[str, torch.device, None]): The device to run the model on.
+
+        The device can be specified as a string, a torch.device, or left as None to use the default device.
+        """
         self.name = name
         self.task = task
         super().__init__(device=device)
 
     def _validate_args(self):
+        """
+        Validate the provided arguments.
+
+        Raises:
+            ValueError: If the model name is None.
+        """
         if self.name is None:
             raise ValueError(f"Model {self.name} not found")
 
     def _load_model(self):
+        """
+        Load the ALBERT model for text classification.
+        """
         self.model = pipeline("text-classification", model=self.name, top_k=None)
 
-    def _preprocess(self, input_text):
+    def _preprocess(self, input_text) -> str:
+        """
+        Preprocess the input text.
+
+        Args:
+            input_text: The input text to preprocess.
+
+        Returns:
+            str: The preprocessed input text as a string.
+        """
         return str(input_text)
 
-    def _predict(self, input_text, **kwargs):
+    def _predict(self, input_text, **kwargs) -> List[Any | None]:
+        """
+        Predict the intent of the input text.
+
+        Args:
+            input_text: The input text for intent classification.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            list[Any | None]: A list of classification results.
+        """
         cls_output = self.model(input_text, **kwargs)
         return cls_output
 
-    def _postprocess(self, predictions):
+    def _postprocess(self, predictions) -> List[Tuple[Any, Any]]:
+        """
+        Postprocess the classification predictions.
+
+        Args:
+            predictions: The raw classification predictions.
+
+        Returns:
+            List[Tuple[Any, Any]]: A list of tuples containing intent class and confidence score.
+        """
         results = []
         for prediction in predictions[0]:
             intent_class = prediction["label"]
@@ -39,7 +94,17 @@ class UDIntentClassification(UDBase):
             results.append((intent_class, intent_score))
         return results
 
-    def __call__(self, input_text, **kwargs):
+    def __call__(self, input_text, **kwargs) -> List[Tuple[Any, Any]]:
+        """
+        Make an intent classification prediction.
+
+        Args:
+            input_text: The input text for intent classification.
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            List[Tuple[Any, Any]]: A list of tuples containing intent class and confidence score.
+        """
         input_text = self._preprocess(input_text)
         predictions = self._predict(input_text, **kwargs)
         return self._postprocess(predictions)
