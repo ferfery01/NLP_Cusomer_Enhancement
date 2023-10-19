e0d9bf35e7f1696d0319e0cc7fb262d0c817d9fd
Diwakar Singh <diwakariitd@gmail.com>
2023-10-19 13:55:11 -0500

    Final touches

--- unified_desktop/pipelines/ser.py
+++ unified_desktop/pipelines/ser.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from typing import NamedTuple, Optional, Union
+from typing import Mapping, NamedTuple, Optional, Union
 
 import torch
 from speechbrain.pretrained.interfaces import foreign_class
 
 from unified_desktop import CACHE_DIR
 from unified_desktop.pipelines.base import UDBase
+
+EMOTIONS_MAP: Mapping[str, str] = {"ang": "Anger", "hap": "Happy", "neu": "Neutral", "sad": "Sad"}
+"""A mapping from the emotion labels to the corresponding human-readable labels.
+"""
 
 
 class EmotionPredictions(NamedTuple):
@@ -14,11 +18,8 @@ class EmotionPredictions(NamedTuple):
     logits: torch.Tensor
     """The log posterior probabilities of each class ([1, N_class])
     """
-    score: torch.Tensor
+    score: float
     """Value of the log-posterior for the best class
-    """
-    cls_index: torch.Tensor
-    """The indexes of the best class
     """
     label: str
     """The emotion label of the best class
@@ -28,38 +29,42 @@ class EmotionPredictions(NamedTuple):
 class UDSpeechEmotionRecognition(UDBase):
     """Detect emotions in speech using SpeechBrain's emotion recognition model.
 
+    Currently, the model predicts one of the following emotion: Anger, Happy, Neutral, Sad
+
     Attributes:
-       device (str, torch.device): PyTorch device for the model. If None, defaults to the best
+        device (str, torch.device): PyTorch device for the model. If None, defaults to the best
            available device.
+        model (EncoderWav2vec2Classifier): The loaded SpeechBrain emotion recognition model.
     """
 
     def __init__(self, device: Optional[Union[str, torch.device]] = None) -> None:
         super().__init__(device=device)
 
     def _load_model(self) -> None:
-        """Load the emotion recognition model."""
+        """Load the speech emotion recognition model."""
         self.model = foreign_class(
             source="speechbrain/emotion-recognition-wav2vec2-IEMOCAP",
             pymodule_file="custom_interface.py",
             classname="CustomEncoderWav2vec2Classifier",
             savedir=CACHE_DIR,
-        )
+        ).to(self.device)
+        self.model.eval()
 
     def _preprocess(self, audio_file: Union[str, Path]) -> str:
         """Convert the audio file path to a string."""
         return str(audio_file)
 
     def _predict(self, audio_file: str) -> EmotionPredictions:
-        """Run the ASR model on the audio file."""
+        """Run the speech emotion recognition model on the audio file."""
         pred = self.model.classify_file(audio_file)
-        return EmotionPredictions(pred[0], pred[1], pred[2], pred[3][0])
+        return EmotionPredictions(pred[0], pred[1].item(), pred[3][0])
 
-    def _postprocess(self, predictions: EmotionPredictions) -> str:
-        """Extract the transcribed text from the ASR predictions."""
-        return predictions.label
+    def _postprocess(self, predictions: EmotionPredictions) -> EmotionPredictions:
+        """Extract the emotion label from the predictions."""
+        return EmotionPredictions(predictions.logits, predictions.score, EMOTIONS_MAP[predictions.label])
 
-    def __call__(self, audio_file: Union[str, Path]) -> str:
+    def __call__(self, audio_file: Union[str, Path]) -> EmotionPredictions:
+        """Perform speech emotion recognition on the audio file and return the emotion label."""
         audio_file_str = self._preprocess(audio_file)
         predictions = self._predict(audio_file_str)
-        transcribed_text = self._postprocess(predictions)
-        return transcribed_text
+        return self._postprocess(predictions)
