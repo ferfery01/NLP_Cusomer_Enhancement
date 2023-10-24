526369d8b9d3f495b1f92cf6e0750f5a720f5c0f
zeinab.takbiri <zeinab.takbiri@walgreens.com>
2023-10-24 11:19:43 -0500

    test

--- unified_desktop/pipelines/__init__.py
+++ unified_desktop/pipelines/__init__.py
@@ -1,7 +1,4 @@
 from unified_desktop.pipelines.asr import UDSpeechRecognizer  # noqa
 from unified_desktop.pipelines.intent import UDIntentClassification  # noqa
-<<<<<<< HEAD
-from unified_desktop.pipelines.sentiment import UDSentimentDetector  # noqa
-=======
 from unified_desktop.pipelines.keyword import UDKeyExtraction  # noqa
->>>>>>> e436254 (keyword)
+from unified_desktop.pipelines.sentiment import UDSentimentDetector  # noqa
