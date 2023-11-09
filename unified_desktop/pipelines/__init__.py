55d5880c89b5f60b3da33b80d1c85bffffd4418a
zeinab.takbiri <zeinab.takbiri@walgreens.com>
2023-11-09 11:25:43 -0600

    change the keyword model

--- unified_desktop/pipelines/__init__.py
+++ unified_desktop/pipelines/__init__.py
@@ -1,6 +1,7 @@
 from unified_desktop.pipelines.asr import UDSpeechRecognizer  # noqa
 from unified_desktop.pipelines.intent import UDIntentClassifier  # noqa
 from unified_desktop.pipelines.keywords import UDKeyExtractor  # noqa
+from unified_desktop.pipelines.keywords2 import UDKeyExtractor2  # noqa
 from unified_desktop.pipelines.sentiment import UDSentimentDetector  # noqa
 from unified_desktop.pipelines.ser import UDSpeechEmotionRecognizer  # noqa
 from unified_desktop.pipelines.summarization import UDSummarizer  # noqa
